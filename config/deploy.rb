079d7d10f672e1cf023f472ef2a5e16a4bf95d04
Faishal <faishalhussain22@gmail.com>
2016-03-22 13:39:24 +0530

    mod

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repo_url, 'git@github.com:PebblesCode/puma_test.git'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/home/deploy/puma'
 
 # Default value for :scm is :git
 # set :scm, :git
